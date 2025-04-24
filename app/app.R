98959cbf7fe3196bc3d1ce88cde0d8c3c65e3db1
mariyakoval <mariyakoval787@gmail.com>
2025-04-24 11:19:37 -0400

    app continent plot

--- app/app.R
+++ app/app.R
@@ -10,6 +10,7 @@ pacman::p_load(tidyverse, ggplot2, lubridate, dplyr, kableExtra, sf,
 
 # --------------- import data 
 climate <- readRDS("Data/climate.rds")
+data <- readRDS("Data/data.rds")
 #get only countries with at least one non NA value for any indicator
 valid_countries <- climate|>
   pivot_longer(cols = matches("^(19|20)\\d{2}$"), names_to = "year", values_to = "value") |>
@@ -17,6 +18,13 @@ valid_countries <- climate|>
   summarise(non_na_count = sum(!is.na(value)))|>
   filter(non_na_count > 0)|>
   pull(`Country Name`)
+
+valid_medians <- data|>
+  pivot_longer(cols = matches("^(19|20)\\d{2}_median$"), names_to = "year", values_to = "value") |>
+  group_by(`region_un`)|>
+  summarise(non_na_count = sum(!is.na(value)))|>
+  filter(non_na_count > 0)|>
+  pull(`region_un`)
 
 # =============================================================================
 # User interface 
@@ -28,6 +36,8 @@ ui <- dashboardPage(
   dashboardSidebar(collapsed = TRUE,
                    sidebarMenu(
                      menuItem("Line Graph", tabName = "line", 
+                              icon = icon("chart-line")),
+                     menuItem("Line Graph by Continent", tabName = "line-region", 
                               icon = icon("chart-line"))
   )),
   
@@ -61,6 +71,35 @@ ui <- dashboardPage(
                        plotlyOutput("regionPlot")
                 )
               )
+      ),
+      tabItem(tabName = "line-region",
+              fluidRow(
+                column(12, h2("Regional Trends by Continent and Indicator"), align = "center")
+              ),
+              fluidRow(
+                column(12,
+                       p("Explore regional trends based on continent selection and indicator of interest.",
+                         style = "font-size: 12px"),
+                       align = "center")
+              ),
+              fluidRow(
+                column(4,
+                       selectizeInput("country_select_con", "Choose a Continent:",
+                                      choices = sort(valid_medians))
+                ),
+                column(4,
+                       selectizeInput(
+                         "indicator_select_con",
+                         "Choose an Indicator:",
+                         choices = sort(unique(data$`Indicator Name`))
+                       )
+                )
+              ),
+              fluidRow(
+                column(12,
+                       plotlyOutput("continentPlot")
+                )
+              )
       )
     )
   )
@@ -70,6 +109,37 @@ ui <- dashboardPage(
 # Server
 # =============================================================================
 server <- function(input, output, session) {
+  
+  output$continentPlot <- renderPlotly({
+    req(input$country_select_con, input$indicator_select_con)
+    
+    #filter data by selected country and indicator
+    filtered_data1 <- data |>
+      filter(`region_un` == input$country_select_con,
+             `Indicator Name` == input$indicator_select_con)
+    
+    #convert year columns to long format
+    data_long <- filtered_data1 |>
+      pivot_longer(
+        cols = matches("^(19|20)\\d{2}_median$"),
+        names_to = "year",
+        values_to = "value"
+      )
+    
+    #generate plot
+    p1 <- ggplot(data_long, aes(x = year, y = value)) +
+      geom_line(color = "darkblue", size = 1) +
+      geom_point(color = "darkblue", size = 2) +
+      labs(
+        title = paste("Variable:", input$indicator_select_con),
+        subtitle = paste("Continent:", input$country_select_con),
+        x = "Year",
+        y = input$indicator_select_con
+      ) +
+      theme_minimal()
+    
+    ggplotly(p1)
+  })
   
   observe({
     req(input$country_select)
